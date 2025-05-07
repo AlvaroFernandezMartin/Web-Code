61fb004581e13430611aeae254a43f5b53399405
Alvaro <alvarofernandezmartintr@gmail.com>
2025-05-07 14:58:32 +0100

    .

--- products/models.py
+++ products/models.py
@@ -12,7 +12,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='products/')
+    image = models.ImageField(upload_to='products/', max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
