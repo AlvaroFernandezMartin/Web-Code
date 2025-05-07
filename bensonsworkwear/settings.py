871e3a2ec72ff60d2e65d4d4c1af983d92c39dd1
Alvaro <alvarofernandezmartintr@gmail.com>
2025-05-07 15:26:27 +0100

    .

--- bensonsworkwear/settings.py
+++ bensonsworkwear/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'products.apps.ProductsConfig',
     'pages.apps.PagesConfig',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # MIDDLEWARE
 MIDDLEWARE = [
@@ -40,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 # URLS
